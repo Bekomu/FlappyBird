7f0b429220069e467ad3b49d467519f910868a28
Berkay <bozisik93@gmail.com>
2022-02-08 09:15:40 +0300

    Add project files.

---
+++ FlappyBird/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace FlappyBird
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbBird = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pbPipe1 = new System.Windows.Forms.PictureBox();
+            this.pbPipe2 = new System.Windows.Forms.PictureBox();
+            this.pbPipe3 = new System.Windows.Forms.PictureBox();
+            this.pbPipe4 = new System.Windows.Forms.PictureBox();
+            this.tmrGame = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPuan = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPipe1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPipe2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPipe3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPipe4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbBird
+            // 
+            this.pbBird.Image = global::FlappyBird.Properties.Resources.birdnew;
+            this.pbBird.Location = new System.Drawing.Point(12, 185);
+            this.pbBird.Name = "pbBird";
+            this.pbBird.Size = new System.Drawing.Size(81, 54);
+            this.pbBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbBird.TabIndex = 1;
+            this.pbBird.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pictureBox1.Image = global::FlappyBird.Properties.Resources.ground;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 583);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1073, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pbPipe1
+            // 
+            this.pbPipe1.Image = global::FlappyBird.Properties.Resources.pipedown;
+            this.pbPipe1.Location = new System.Drawing.Point(402, 0);
+            this.pbPipe1.Name = "pbPipe1";
+            this.pbPipe1.Size = new System.Drawing.Size(89, 239);
+            this.pbPipe1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbPipe1.TabIndex = 2;
+            this.pbPipe1.TabStop = false;
+            // 
+            // pbPipe2
+            // 
+            this.pbPipe2.Image = global::FlappyBird.Properties.Resources.pipe;
+            this.pbPipe2.Location = new System.Drawing.Point(491, 403);
+            this.pbPipe2.Name = "pbPipe2";
+            this.pbPipe2.Size = new System.Drawing.Size(94, 181);
+            this.pbPipe2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbPipe2.TabIndex = 3;
+            this.pbPipe2.TabStop = false;
+            // 
+            // pbPipe3
+            // 
+            this.pbPipe3.Image = global::FlappyBird.Properties.Resources.pipedown;
+            this.pbPipe3.Location = new System.Drawing.Point(708, 0);
+            this.pbPipe3.Name = "pbPipe3";
+            this.pbPipe3.Size = new System.Drawing.Size(89, 205);
+            this.pbPipe3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbPipe3.TabIndex = 4;
+            this.pbPipe3.TabStop = false;
+            // 
+            // pbPipe4
+            // 
+            this.pbPipe4.Image = global::FlappyBird.Properties.Resources.pipe;
+            this.pbPipe4.Location = new System.Drawing.Point(817, 354);
+            this.pbPipe4.Name = "pbPipe4";
+            this.pbPipe4.Size = new System.Drawing.Size(89, 230);
+            this.pbPipe4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbPipe4.TabIndex = 5;
+            this.pbPipe4.TabStop = false;
+            // 
+            // tmrGame
+            // 
+            this.tmrGame.Enabled = true;
+            this.tmrGame.Interval = 20;
+            this.tmrGame.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(22, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 31);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Points :";
+            // 
+            // lblPuan
+            // 
+            this.lblPuan.AutoSize = true;
+            this.lblPuan.BackColor = System.Drawing.Color.Transparent;
+            this.lblPuan.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblPuan.ForeColor = System.Drawing.Color.Red;
+            this.lblPuan.Location = new System.Drawing.Point(146, 19);
+            this.lblPuan.Name = "lblPuan";
+            this.lblPuan.Size = new System.Drawing.Size(29, 31);
+            this.lblPuan.TabIndex = 7;
+            this.lblPuan.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1073, 633);
+            this.Controls.Add(this.lblPuan);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbPipe4);
+            this.Controls.Add(this.pbPipe3);
+            this.Controls.Add(this.pbPipe2);
+            this.Controls.Add(this.pbPipe1);
+            this.Controls.Add(this.pbBird);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pbBird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPipe1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPipe2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPipe3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPipe4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pbBird;
+        private System.Windows.Forms.PictureBox pbPipe1;
+        private System.Windows.Forms.PictureBox pbPipe2;
+        private System.Windows.Forms.PictureBox pbPipe3;
+        private System.Windows.Forms.PictureBox pbPipe4;
+        private System.Windows.Forms.Timer tmrGame;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPuan;
+    }
+}
+
---
+++ FlappyBird/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlappyBird
+{
+    public partial class Form1 : Form
+    {
+        int yerCekimi = 5;
+        int hiz = 7;
+        Random rnd = new Random();
+        int score = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            score += 1;
+
+                if (score > 0)
+                {
+                    lblPuan.Text = Convert.ToString(score);
+
+                }
+            pbBird.Top += yerCekimi;
+
+
+            if (pbBird.Bottom == pictureBox1.Top)
+            {
+                tmrGame.Stop();
+            }
+            pbPipe1.Left -= hiz;
+            pbPipe2.Left -= hiz;
+            pbPipe3.Left -= hiz;
+            pbPipe4.Left -= hiz;
+
+
+
+            if (pbPipe1.Right <= 0)
+            {
+                pbPipe1.Left = ClientSize.Width + rnd.Next(200);
+            }
+            if (pbPipe2.Right <= 0)
+            {
+                pbPipe2.Left = ClientSize.Width + rnd.Next(200);
+            }
+            if (pbPipe3.Right <= 0)
+            {
+                pbPipe3.Left = pbPipe1.Left + rnd.Next(200) + pbPipe1.Width;
+            }
+
+            if (pbPipe4.Right <= 0)
+            {
+                pbPipe4.Left = pbPipe2.Left + rnd.Next(200) + pbPipe2.Width;
+            }
+
+            if (pbPipe1.Bounds.IntersectsWith(pbBird.Bounds) || pbPipe2.Bounds.IntersectsWith(pbBird.Bounds)
+                || pbPipe3.Bounds.IntersectsWith(pbBird.Bounds) || pbPipe4.Bounds.IntersectsWith(pbBird.Bounds)
+                 || pictureBox1.Bounds.IntersectsWith(pbBird.Bounds)) // 4 boru ve zemin için...
+            {
+                tmrGame.Stop();
+                DialogResult dr = MessageBox.Show($" Points : {score} \r\n GAME OVER! \r\n Do you want to play again ? ", "Flappy Bird", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dr == DialogResult.Yes)
+                {
+                    tmrGame.Start();
+                    pbBird.Left = 10;
+                    pbBird.Top = 200;
+                    score = 0;
+                    pbPipe1.Left = ClientSize.Width;
+                    pbPipe2.Left = ClientSize.Width;
+                    pbPipe3.Left = pbPipe1.Left + rnd.Next(200) + pbPipe1.Width;
+                    pbPipe4.Left = pbPipe2.Left + rnd.Next(200) + pbPipe2.Width;
+
+                }
+                else
+                {
+                    Close();
+                }
+
+
+
+
+
+            }
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space && tmrGame.Enabled)
+            {
+                if (pbBird.Top > 0)
+                {
+                    pbBird.Top -= 50;
+                    pbBird.Top = pbBird.Top < 0 ? 0 : pbBird.Top;    // ekrandan taşmaması için...
+                }
+
+            }
+
+
+
+        }
+
+    }
+}
